731c465ad2f645f32f52f8ef0047c80ff13754cb
ushita37 <ushita37@gmail.com>
2024-01-05 18:27:31 +0900

    Add 1-2.md on Jan 5

---
+++ iOS-App-1-3/iOS-App-1-3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-App-1-3
+//
+//  Created by うしたウシタ on 2024/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-App-1-3/iOS-App-1-3/iOS_App_1_3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_1_3App.swift
+//  iOS-App-1-3
+//
+//  Created by うしたウシタ on 2024/01/05.
+//
+
+import SwiftUI
+
+@main
+struct iOS_App_1_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
