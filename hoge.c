5e57060b6144313da7e5e35d7bd79ec9a050e324
ushita37 <ushita37@gmail.com>
2024-01-10 00:21:00 +0900

    Jan 9 (Jan 10) Regular Expression

--- hoge.c
+++ hoge.c
@@ -1,5 +1,10 @@
 1 #include <stdio.h>
 2 #include <stdlib.h>
-3 char buf[128];
-4
-5 struct student { int id; char name[32]; int score; };
+3 #include <string.h>
+4 char buf[128];
+5
+6 int main(void) {
+7   fgets(buf, sizeof(buf), stdin); // 正規表現^\s+で、連続する空白も削除できる
+8   printf("size: %d, %s",sizeof(buf), buf);
+9   return 0;
+10 }    // 正規表現^[\d]+で、2桁以上の数字を削除できる
