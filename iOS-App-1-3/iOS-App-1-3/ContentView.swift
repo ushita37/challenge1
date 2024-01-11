652ed65d47b80f0bc5eb890c5a10a14dae9d6fc9
ushita37 <ushita37@gmail.com>
2024-01-11 23:42:19 +0900

    Jan 11 random & spacer rock-paper-scissors

--- iOS-App-1-3/iOS-App-1-3/ContentView.swift
+++ iOS-App-1-3/iOS-App-1-3/ContentView.swift
@@ -15,10 +15,13 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
+            // スペースを追加
+            Spacer()
             
             // 数字が0だったら
             if jankenNumber == 0 {
                 Text("これからじゃんけんをします！")
+                    .padding(.bottom)
             } else if jankenNumber == 1 {
                 // 数字が1だったらグー
                 // グー画像を指定
@@ -26,30 +29,51 @@ struct ContentView: View {
                 // モディファイア(画面に収まるよう縦横比を維持してリサイズ)
                     .resizable()
                     .scaledToFit()
+                Spacer()
                 Text("グー")
+                    .padding(.bottom)
             } else if jankenNumber == 2 {
                 // 数字が2だったらチョキ
                 Image(.choki)
                     .resizable()
                     .scaledToFit()
+                Spacer()
                 Text("チョキ")
+                    .padding(.bottom)
             } else {
                 // パー
                 Image(.pa)
                     .resizable()
                     .scaledToFit()
+                Spacer()
                 Text("パー")
-                
+                    .padding(.bottom)
             }
             
 
             
             Button(action: {
                 // 次のじゃんけんへ
-                jankenNumber = jankenNumber + 1
+                jankenNumber = Int.random(in: 1...3)
+                // 新しいじゃんけんの結果
+                var newJankenNumber = 0
+                
+                // 前回の結果と異なる場合のみ採用
+                repeat {
+                    newJankenNumber = Int.random(in: 1...3)
+                    // 前回と同じ結果の時は、再度ランダムに数値を出す
+                } while jankenNumber == newJankenNumber
+                
+                // 新しいじゃんけんの結果を格納
+                jankenNumber = newJankenNumber
             }, label: {
                 
                 Text("じゃんけんをする！")
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .frame(height: 100)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .background(.pink)
+                    .foregroundColor(.white)
             })
         }
     }
